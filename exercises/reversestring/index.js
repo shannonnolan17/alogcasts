cc481431bb62eda87edc4279e5636819c4c75212
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-08 16:56:24 -0700

    Complete first exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed
+
+}
 
 module.exports = reverse;
+
+// another Example
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join('');
+// }
