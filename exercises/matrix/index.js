75244fc5ea501454f8b014356a683c53ff38583e
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-10 15:20:20 -0700

    Add another solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -16,14 +16,48 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-  counter = 1;
-  results = [];
+  const results = [];
+
   for (let i = 0; i < n; i++) {
-    results.push(new Array);
+    results.push([]);
   }
 
-  if ()
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
 
+    // Right column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    // Bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    // start column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+
+  return results;
 }
 
 module.exports = matrix;
