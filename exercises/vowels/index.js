d14071a21e439edf4c23ec04163ff5cf8e6ba806
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-10 13:37:18 -0700

    Add another solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,14 +8,8 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-  vowelCount = 0;
-  vowels = ['a', 'e','i', 'o','u']
-  for (let char of str.toLowerCase()) {
-    if (vowels.includes(char)) {
-      vowelCount += 1
-    }
-  }
-  return vowelCount;
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
